796c7838e55e4f1d9d2c568efb166eaaff64840c
Ilyas Sarsenbaev <ilyastuit@gmail.com>
2021-06-23 22:40:49 +0500

    added DataSourceConfig and PropertyConfig classes

---
+++ src/main/java/com/epam/esm/config/DataSourceConfig.java
@@ -0,0 +1,40 @@
+package com.epam.esm.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Value("${datasource.driver}")
+    private String datasourceDriver;
+    @Value("${datasource.url}")
+    private String datasourceUrl;
+    @Value("${datasource.username}")
+    private String datasourceUsername;
+    @Value("${datasource.password}")
+    private String datasourcePassword;
+
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        System.err.println(this.datasourceDriver);
+        dataSource.setDriverClassName(this.datasourceDriver);
+        dataSource.setUrl(this.datasourceUrl);
+        dataSource.setUsername(this.datasourceUsername);
+        dataSource.setPassword(this.datasourcePassword);
+
+        return dataSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
+    }
+}
