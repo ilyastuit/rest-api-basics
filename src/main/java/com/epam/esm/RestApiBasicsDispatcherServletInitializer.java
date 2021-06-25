0245bbc7a62a7be5fe1415ef477d69f993bd75da
Ilyas Sarsenbaev <ilyastuit@gmail.com>
2021-06-25 13:59:46 +0500

    cleaning unused code

--- src/main/java/com/epam/esm/RestApiBasicsDispatcherServletInitializer.java
+++ src/main/java/com/epam/esm/RestApiBasicsDispatcherServletInitializer.java
@@ -1,7 +1,5 @@
 package com.epam.esm;
 
-import com.epam.esm.config.DataSourceConfig;
-import com.epam.esm.config.PropertyConfig;
 import com.epam.esm.config.ServletConfig;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
