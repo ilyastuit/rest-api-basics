88f460526a4acf13c352feefbc15107b4dc59ae8
Ilyas Sarsenbaev <ilyastuit@gmail.com>
2021-06-25 13:58:50 +0500

    added NamedPropertyJdbcTemplate bean

--- src/main/java/com/epam/esm/config/DataSourceConfig.java
+++ src/main/java/com/epam/esm/config/DataSourceConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
@@ -36,5 +37,10 @@ public class DataSourceConfig {
     @Bean
     public JdbcTemplate jdbcTemplate() {
         return new JdbcTemplate(dataSource());
+    }
+
+    @Bean
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate() {
+        return new NamedParameterJdbcTemplate(dataSource());
     }
 }
