8bd4ec5361b93b1830ff313147c95e51babaaa53
Ilyas Sarsenbaev <ilyastuit@gmail.com>
2021-06-26 12:12:14 +0500

    updated service and controller

--- src/main/java/com/epam/esm/api/v1/GiftCertificateController.java
+++ src/main/java/com/epam/esm/api/v1/GiftCertificateController.java
@@ -2,64 +2,120 @@ package com.epam.esm.api.v1;
 
 import com.epam.esm.domain.giftcertificate.GiftCertificate;
 import com.epam.esm.domain.giftcertificate.validation.GiftCertificateApiErrors;
+import com.epam.esm.service.CheckerUtil;
 import com.epam.esm.service.GiftCertificateService;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.epam.esm.service.exceptions.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.DataBinder;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
-import org.springframework.validation.Validator;
+import org.springframework.validation.*;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
 @RestController
-@RequestMapping("api/v1/gift-certificates")
+@RequestMapping("/api/v1/gift-certificates")
 public class GiftCertificateController {
 
     private final GiftCertificateService giftCertificateService;
     private final Validator giftCertificateValidator;
 
-    @Autowired
     public GiftCertificateController(GiftCertificateService giftCertificateService, Validator giftCertificateValidator) {
         this.giftCertificateService = giftCertificateService;
         this.giftCertificateValidator = giftCertificateValidator;
     }
 
     @GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> all(@RequestParam("withTags") Optional<String> withTags){
-        if (withTags.isPresent() && Boolean.parseBoolean(withTags.get())) {
-            return new ResponseEntity<>(giftCertificateService.getAllWithTags(), HttpStatus.OK);
+    public ResponseEntity<?> all(@RequestParam("tags") Optional<String> tags,
+                                 @RequestParam("date") Optional<String> date,
+                                 @RequestParam("name") Optional<String> name) {
+
+        return new ResponseEntity<>(this.giftCertificateService.getAll(tags, date, name), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> one(@PathVariable("id") int id, @RequestParam("tags") Optional<String> tags) {
+        boolean withTags = CheckerUtil.isValidBoolean(tags);
+
+        try {
+            return new ResponseEntity<>(this.giftCertificateService.getOne(id, withTags), HttpStatus.OK);
+        } catch (NotFoundException exception) {
+            return new ResponseEntity<>("GiftCertificate with id = " + id + " is not found.", HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<>(giftCertificateService.getAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/search", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> search(@RequestParam("q") Optional<String> q,
+                                    @RequestParam("tag") Optional<String> tag,
+                                    @RequestParam("date") Optional<String> date,
+                                    @RequestParam("name") Optional<String> name) {
+        if (q.isPresent()) {
+            return new ResponseEntity<>(this.giftCertificateService.getAllByNameOrDescription(q.get(), date, name), HttpStatus.OK);
+        } else if(tag.isPresent()) {
+            return new ResponseEntity<>(this.giftCertificateService.getAllByTagName(tag.get()), HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Please provide the search parameter <q>", HttpStatus.BAD_REQUEST);
     }
 
     @PostMapping(value = "/create", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> create(@RequestBody GiftCertificate giftCertificate) {
 
+        final BindingResult bindingResult = validate(giftCertificate, this.giftCertificateValidator);
+
+        if (bindingResult.hasErrors()) {
+            return returnApiErrors(bindingResult);
+        }
+
+        return new ResponseEntity<>(this.giftCertificateService.save(giftCertificate), HttpStatus.CREATED);
+    }
+
+    @PatchMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody GiftCertificate giftCertificate) {
+
+        if (!this.giftCertificateService.isExistById(id)) {
+            return new ResponseEntity<>("GiftCertificate with id = " + id + " is not found.", HttpStatus.NOT_FOUND);
+        }
+
+        final BindingResult bindingResult = validate(giftCertificate, this.giftCertificateValidator);
+
+        if (bindingResult.hasErrors()) {
+            return returnApiErrors(bindingResult);
+        }
+
+        GiftCertificate result = null;
+        try {
+            result = this.giftCertificateService.getOne(this.giftCertificateService.update(id, giftCertificate), true);
+        } catch (NotFoundException exception) {
+            return new ResponseEntity<>("Error in the process of updating.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") int id) {
+        this.giftCertificateService.delete(id);
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    public ResponseEntity<?> returnApiErrors(BindingResult bindingResult) {
+        Map<String, String> errors = new HashMap<>();
+
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        for (ObjectError error : bindingResult.getGlobalErrors()) {
+            errors.put(error.getObjectName(), error.getDefaultMessage());
+        }
+
+        GiftCertificateApiErrors result = new GiftCertificateApiErrors(HttpStatus.BAD_REQUEST, GiftCertificateApiErrors.DEFAULT_ERROR_MESSAGE, errors);
+        return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
+    }
+
+    private BindingResult validate(GiftCertificate giftCertificate, Validator validator) {
         final DataBinder dataBinder = new DataBinder(giftCertificate);
         dataBinder.addValidators(this.giftCertificateValidator);
         dataBinder.validate();
-
-        if (dataBinder.getBindingResult().hasErrors()) {
-            Map<String, String> errors = new HashMap<>();
-
-            for(FieldError fieldError : dataBinder.getBindingResult().getFieldErrors()) {
-                errors.put(fieldError.getField(), fieldError.getDefaultMessage());
-            }
-            for (ObjectError error : dataBinder.getBindingResult().getGlobalErrors()) {
-                errors.put(error.getObjectName(), error.getDefaultMessage());
-            }
-
-            GiftCertificateApiErrors result = new GiftCertificateApiErrors(HttpStatus.BAD_REQUEST, GiftCertificateApiErrors.DEFAULT_ERROR_MESSAGE, errors);
-            return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
-        }
-
-        return new ResponseEntity<>(giftCertificate, HttpStatus.OK);
+        return dataBinder.getBindingResult();
     }
-
 }
--- src/main/java/com/epam/esm/service/GiftCertificateService.java
+++ src/main/java/com/epam/esm/service/GiftCertificateService.java
@@ -3,9 +3,13 @@ package com.epam.esm.service;
 import com.epam.esm.domain.giftcertificate.GiftCertificate;
 import com.epam.esm.repository.GiftCertificateRepository;
 import com.epam.esm.service.exceptions.NotFoundException;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class GiftCertificateService {
@@ -15,35 +19,143 @@ public class GiftCertificateService {
         this.certificateRepository = certificateRepository;
     }
 
-    public GiftCertificate getOne(int id) throws NotFoundException {
-        return getFromList(certificateRepository.findById(id), id);
+    public int save(GiftCertificate giftCertificate) {
+        giftCertificate.setCreateDate(LocalDateTime.now());
+        giftCertificate.setLastUpdateDate(LocalDateTime.now());
+
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("name", giftCertificate.getName());
+        params.addValue("description", giftCertificate.getDescription());
+        params.addValue("price", giftCertificate.getPrice());
+        params.addValue("duration", giftCertificate.getDuration());
+        params.addValue("create_date", Timestamp.valueOf(giftCertificate.getCreateDate()));
+        params.addValue("last_update_date", Timestamp.valueOf(giftCertificate.getLastUpdateDate()));
+
+        if (giftCertificate.getTags() != null && !giftCertificate.getTags().isEmpty()) {
+            params.addValue("tags", giftCertificate.getTags());
+        }
+
+        return this.certificateRepository.save(params);
+    }
+
+    public int update(int id, GiftCertificate giftCertificate) {
+        giftCertificate.setLastUpdateDate(LocalDateTime.now());
+
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("name", giftCertificate.getName());
+        params.addValue("description", giftCertificate.getDescription());
+        params.addValue("price", giftCertificate.getPrice());
+        params.addValue("duration", giftCertificate.getDuration());
+        params.addValue("last_update_date", Timestamp.valueOf(giftCertificate.getLastUpdateDate()));
+
+        if (giftCertificate.getTags() != null && !giftCertificate.getTags().isEmpty()) {
+            params.addValue("tags", giftCertificate.getTags());
+        }
+
+        return this.certificateRepository.update(id, params);
+    }
+
+    public boolean isExistById(int id) {
+        return this.certificateRepository.isExistById(id);
+    }
+
+    public GiftCertificate getOne(int id, boolean withTags) throws NotFoundException {
+        if (withTags) {
+            return getFromList(this.certificateRepository.findByIdWithTags(id), id);
+        }
+        return getFromList(this.certificateRepository.findById(id), id);
     }
 
     public GiftCertificate getOneWithTags(int id) throws NotFoundException {
-        return getFromList(certificateRepository.findByIdWithTags(id), id);
+        return getFromList(this.certificateRepository.findByIdWithTags(id), id);
     }
 
-    public List<GiftCertificate> getAll() {
-        return certificateRepository.findAll();
+    public List<GiftCertificate> getAll(Optional<String> tags, Optional<String> date, Optional<String> name) {
+        boolean withTags = CheckerUtil.isValidBoolean(tags);
+        boolean sortByDate = CheckerUtil.isValidSort(date);
+        boolean sortByName = CheckerUtil.isValidSort(name);
+
+        if (sortByDate && !sortByName) {
+            return this.getAllSortByDate(withTags, date.get());
+        } else if (!sortByDate && sortByName) {
+            return this.getAllSortByName(withTags, name.get());
+        } else if (sortByDate && sortByName) {
+            return this.getAllSortByDateAndByName(withTags, date.get(), name.get());
+        } else {
+            if (withTags) {
+                return this.getAllWithTags();
+            }
+            return this.certificateRepository.findAll();
+        }
+    }
+
+
+    public List<GiftCertificate> getAllSortByDate(boolean withTags, String date) {
+        if (withTags) {
+            return this.certificateRepository.findAllWithTagsOrderByDate(date);
+        }
+        return this.certificateRepository.findAllOrderByDate(date);
+    }
+
+
+    public List<GiftCertificate> getAllSortByName(boolean withTags, String name) {
+        if (withTags) {
+            return this.certificateRepository.findAllWithTagsOrderByName(name);
+        }
+        return this.certificateRepository.findAllOrderByName(name);
+    }
+
+    public List<GiftCertificate> getAllSortByDateAndByName(boolean withTags, String date, String name) {
+        if (withTags) {
+            return this.certificateRepository.findAllWithTagsOrderByDateAndByName(date, name);
+        }
+        return this.certificateRepository.findAllOrderByDateAndByName(date, name);
     }
 
     public List<GiftCertificate> getAllWithTags() {
-        return certificateRepository.findAllWithTags();
+        return this.certificateRepository.findAllWithTags();
     }
 
     public List<GiftCertificate> getAllByTagName(String tagName) {
-        return certificateRepository.findAllByTagName(tagName);
+        return this.certificateRepository.findAllWithTagsByTagName(tagName);
     }
 
-    public List<GiftCertificate> getAllByNameOrDescription(String text) {
-        return certificateRepository.findAllByNameOrDescription(text);
+    public List<GiftCertificate> getAllByNameOrDescription(String text, Optional<String> date, Optional<String> name) {
+        boolean sortByDate = CheckerUtil.isValidSort(date);
+        boolean sortByName = CheckerUtil.isValidSort(name);
+
+        if (sortByDate && !sortByName) {
+            return this.getAllByNameOrDescriptionSortByDate(text, date.get());
+        } else if (!sortByDate && sortByName) {
+            return this.getAllByNameOrDescriptionSortByName(text, name.get());
+        } else if (sortByDate && sortByName) {
+            return this.getAllByNameOrDescriptionSortByDateAndByName(text, date.get(), name.get());
+        } else {
+            return certificateRepository.findAllByNameOrDescription(text);
+        }
+    }
+
+    private List<GiftCertificate> getAllByNameOrDescriptionSortByDateAndByName(String text, String date, String name) {
+        return this.certificateRepository.findAllByNameOrDescriptionOrderByDateAndName(text, date, name);
+    }
+
+    private List<GiftCertificate> getAllByNameOrDescriptionSortByName(String text, String name) {
+        return this.certificateRepository.findAllByNameOrDescriptionOrderByName(text, name);
+    }
+
+    private List<GiftCertificate> getAllByNameOrDescriptionSortByDate(String text, String date) {
+        return this.certificateRepository.findAllByNameOrDescriptionOrderByDate(text, date);
     }
 
     public List<GiftCertificate> getAllByNameOrDescriptionWithTags(String text) {
-        return certificateRepository.findAllByNameOrDescriptionWithTags(text);
+        return this.certificateRepository.findAllWithTagsByNameOrDescription(text);
     }
 
     private GiftCertificate getFromList(List<GiftCertificate> certificateList, int id) throws NotFoundException {
         return certificateList.stream().findAny().orElseThrow(() -> new NotFoundException("GiftCertificate is not found (id = " + id + ")"));
+    }
+
+    public void delete(int id) {
+        this.certificateRepository.deleteById(id);
     }
 }
