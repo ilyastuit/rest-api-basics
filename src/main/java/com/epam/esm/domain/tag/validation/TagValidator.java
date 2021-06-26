c71971e4492168df49c1377358ced793a93b968b
Ilyas Sarsenbaev <ilyastuit@gmail.com>
2021-06-26 13:23:46 +0500

    added unique validation for tag name

--- src/main/java/com/epam/esm/domain/tag/validation/TagValidator.java
+++ src/main/java/com/epam/esm/domain/tag/validation/TagValidator.java
@@ -1,12 +1,19 @@
 package com.epam.esm.domain.tag.validation;
 
 import com.epam.esm.domain.tag.Tag;
+import com.epam.esm.repository.TagRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 @Service
 public class TagValidator implements Validator {
+
+    private final TagRepository tagRepository;
+
+    public TagValidator(TagRepository tagRepository) {
+        this.tagRepository = tagRepository;
+    }
 
     @Override
     public boolean supports(Class<?> clazz) {
@@ -18,6 +25,10 @@ public class TagValidator implements Validator {
         Tag tag = (Tag) target;
         if (tag.getName() == null || tag.getName().isEmpty()) {
             errors.rejectValue("name", "name.required", "Name is required and should not be empty.");
+        }
+
+        if (tagRepository.isNameExist(tag.getName())) {
+            errors.rejectValue("name", "name.unique", "Tag with this name is already exist.");
         }
     }
 }
